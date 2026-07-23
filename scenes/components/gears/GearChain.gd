b99ca2e35ec5a5f4c8a7359f00fc05b29b2c6db2
tijnedens <70977636+tijnedens@users.noreply.github.com>
2026-07-23 21:59:29 +0200

    Tijn/gear signalling (#19)
    * gear inventory item
    
    * other gear sizes and shader fix

--- scenes/components/gears/GearChain.gd
+++ scenes/components/gears/GearChain.gd
@@ -1,6 +1,18 @@
 class_name GearChain
 extends Node
 
+static var gear_ratios = {
+	"SMALLtoBIG": 0.455,
+	"BIGtoSMALL": 2.2,
+	"MEDIUMtoSMALL": 1.8,
+	"SMALLtoMEDIUM": 0.555,
+	"MEDIUMtoBIG": 0.666,
+	"BIGtoMEDIUM": 1.5,
+	"BIGtoBIG": 1.0,
+	"MEDIUMtoMEDIUM": 1.0,
+	"SMALLtoSMALL": 1.0
+}
+
 static func check_chain(gear : GearComponent) -> bool:
 	gear.is_checked = true
 	if gear.next_gears.is_empty():
@@ -9,7 +21,7 @@ static func check_chain(gear : GearComponent) -> bool:
 	for g in gear.next_gears:
 		if gear.is_activated && !g.is_activated:
 			g.is_activated = true
-			g.rotation_speed = -gear.rotation_speed
+			g.rotation_speed = -gear.rotation_speed * gear_ratios[gear.gear_size + "to" + g.gear_size]
 		if gear.is_activated && g.is_activated:
 			if gear.rotation_speed * g.rotation_speed > 0:
 				is_legal = false
--- scenes/components/gears/GearComponent.gd
+++ scenes/components/gears/GearComponent.gd
@@ -2,15 +2,23 @@ class_name GearComponent
 extends BaseComponent
 
 @export_enum("BIG", "MEDIUM", "SMALL") var gear_size = "MEDIUM"
+@export var inventory_item : Node
+## Met de klok mee
+@export_range(0, 360, 1.0, "radians_as_degrees")  var inventory_item_end_angle : float
 
-var is_checked : bool = false
+signal inventory_item_reached
+
+var is_checked : bool = false # used by GearChain class to check if this gear was already checked
 var rotation_speed : float = 1.0
 
-var is_part_of_jammed_chain : bool = false
 var next_gears : Array[GearComponent]
-var activating_gears: Array[GearComponent]
-@export var is_activated : bool = false
+var is_activated : bool = false
 var activation_pulse_stop : bool = false
+var inventory_pulse_stop : bool = false
+
+func _ready():
+	if inventory_item:
+		inventory_item.reparent($GearVisualizer/Sprite)
 
 func _process(_delta):
 	if next_gears.is_empty():
@@ -19,18 +27,22 @@ func _process(_delta):
 		$ConnectionArea/ConnectionShape.debug_color = Color.GREEN
 	
 	if is_activated && !activation_pulse_stop:
-		$BigGearVisualizer.visualize_start(40 * rotation_speed)
-		print("start")
+		$GearVisualizer.visualize_start(20 * rotation_speed)
 		activation_pulse_stop = true
+	
+	var current_animation_rotation : float = $GearVisualizer/Sprite.rotation
+	if inventory_item && !inventory_pulse_stop && (roundf(current_animation_rotation * 10) == roundf(inventory_item_end_angle * 10)):
+		inventory_item_reached.emit()
+		inventory_pulse_stop = true
 
 func show_jam():
-	$BigGearVisualizer.visualize_jam()
+	$GearVisualizer.visualize_jam()
 
 func start() -> void:
 	pass
 
 func reset() -> void:
-	$BigGearVisualizer.reset()
+	$GearVisualizer.reset()
 
 func on_drop() -> void:
 	pass
