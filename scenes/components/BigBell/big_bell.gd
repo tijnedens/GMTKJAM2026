019bcbf01bfc4e45a0d7278a6dce43ab7e4adb2a
tijnedens <tijnedens@gmail.com>
2026-07-26 12:26:21 +0200

    teveel errors, moest er ff wat aan doen

--- scenes/components/BigBell/big_bell.gd
+++ scenes/components/BigBell/big_bell.gd
@@ -2,7 +2,6 @@ class_name kanonComponent
 extends BaseComponent
 
 @onready var big_bell_visualizer: BigBellVisualizer = $BigBellVisualizer
-@onready var static_body_2d: StaticBody2D = $StaticBody2D
 
 
 func _process(_delta):
