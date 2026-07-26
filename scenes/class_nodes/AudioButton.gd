fc64dcf811853d5c8bd2274b180fee8efaf7f6dc
m-vd-v <kramstreep4@gmail.com>
2026-07-26 17:01:24 +0200

    oh nee

--- scenes/class_nodes/AudioButton.gd
+++ scenes/class_nodes/AudioButton.gd
@@ -1,13 +1,17 @@
 class_name AudioButton
 extends Button
 
+@export var custom_audio: AudioStream
 var audio_player: CustomAudioPlayer
 
 func _ready() -> void:
 	audio_player = CustomAudioPlayer.new()
-	audio_player.stream = load(
-		"res://assets/audio/sfx/ui/klik.mp3"
-	)
+	if custom_audio:
+		audio_player.stream = custom_audio
+	else:
+		audio_player.stream = load(
+			"res://assets/audio/sfx/ui/klik.mp3"
+		)
 	audio_player.volume_db = 15
 	add_child(audio_player)
 	pressed.connect(audio_player.play)
